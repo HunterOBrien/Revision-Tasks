5817200f6c1ed3dbc427323fd653f367650c35d2
HunterOBrien <obrienh2@middleton.school.nz>
2024-02-11 21:29:35 +1300

    Update Sandwich Maker Problem_06.py

--- Sandwich Maker Problem_06.py
+++ Sandwich Maker Problem_06.py
@@ -27,19 +27,34 @@ def sandwich_selection(category):
     # Prints each category separately
     print(f"\navailable {category} options: ")
 
-    # for loop
+    # loops through option and its correspondant price for each category separately
     for choice, price in sandwich[category].items():
         print(f"{choice}: ${price}")
+
+    # loops until all three components have been successfully picked
     while counter < 3:
         choice = input(f"Select your {category}: ").strip()
         if choice in sandwich[category]:
             counter += 1
             return choice
+        # if choice not in the category loops again
         else:
             print("Invalid choice. Please try again.")
+
+
+# function to add cost of options together
+def price_adder(bread_choice, meat_choice, garnish_choice):
+    total_price = sandwich["Bread"][bread_choice] + sandwich["Meat"][meat_choice] + sandwich["Garnish"][garnish_choice]
+    return total_price
 
 
 # User choices for each category
 bread_choice = sandwich_selection("Bread")
 meat_choice = sandwich_selection("Meat")
 garnish_choice = sandwich_selection("Garnish")
+
+# total price of burger
+total_burger_price = price_adder(bread_choice, meat_choice, garnish_choice)
+
+print(f"The price for your sandwich ({bread_choice} Bread, {meat_choice}, {garnish_choice}) is: ")
+print(f"${total_burger_price}")
